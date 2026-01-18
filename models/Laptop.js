a8d141c7a19f95db2fff2bb1ccf1afd224b6ccb6
ulnazseitzhan <ulnazzzseitzh@gmail.com>
2026-01-18 19:43:28 +0500

    Add files via upload

---
+++ models/Laptop.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const laptopSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    brand: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+    description: {
+      type: String
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Laptop", laptopSchema);
